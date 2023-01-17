e8b703df430089df9ff8c470d598cec68f8ef49b
subramanyam <subramanyam.katragadda09@gmail.com>
2023-01-17 12:20:30 +0530

    paramsv mdf

--- SeleniumProject/src/Testng/Params.java
+++ SeleniumProject/src/Testng/Params.java
@@ -7,7 +7,7 @@ public class Params {
 	
 	@Test
 	@Parameters({"val1","val2"})
-	void paramsMethod(int a, int b int c)
+	void paramsMethod(int a, int b, int c, int d)
 	{
 		System.out.println(a+b);
 	}
