234e364ed863c38a9608cd411c247c95a5581795
subramanyam <subramanyam.katragadda09@gmail.com>
2023-01-19 12:23:01 +0530

    LIVEPROJECT Commit

---
+++ LIVEPROJECT/src/test/java/PAGES/Hi.java
@@ -0,0 +1,11 @@
+package PAGES;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+
+	}
+
+}
---
+++ LIVEPROJECT/src/test/java/PAGES/homepage.java
@@ -0,0 +1,13 @@
+package PAGES;
+
+import org.openqa.selenium.WebDriver;
+
+public class homepage {
+	 WebDriver driver;
+	 public homepage(WebDriver driver) {
+		 this.driver=driver;
+		 
+	 }
+	 
+
+}
