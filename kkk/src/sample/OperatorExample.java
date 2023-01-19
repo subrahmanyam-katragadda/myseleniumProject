e3a87baf2b0c418ec522d3bf63a5dd2fecc5f702
subramanyam <subramanyam.katragadda09@gmail.com>
2023-01-19 12:16:21 +0530

    kkk Commit

---
+++ kkk/src/sample/OperatorExample.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class OperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=20;
+      
+      int b=-20;
+      
+      boolean c=true;
+      
+      boolean d=false;
+      
+    	  System.out.println(~a);
+    	  System.out.println(~b);
+    	  System.out.println(!c);
+    	  System.out.println(!d);
+    	  
+}
+}
